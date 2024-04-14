d01d4cfc83cbe5e7bd044c47c478c388e338c1b8
JackHunter2 <46602010+JackHunter2@users.noreply.github.com>
2024-04-14 20:32:31 +0700

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,9 @@
+'''
+Tipe Data Booolean
+simpan dengan nama boolean.py
+'''
+narkoba = False
+belajar = True
+
+print(narkoba)
+print(belajar)
---
+++ tipe data.py
@@ -0,0 +1,39 @@
+#tipe data Boolean
+print(True)
+#tipe data String
+print("string dengan menggunakakn tanda kutip dua")
+print('ini string menggunakan tanda kutip satu')
+#tipe data string menjelaskan spesial karakter atau escape sequences
+print('ini adalah tanda single quote(\')')
+print("ini adalah tanda double quote(\")")
+print("ini adalah tanda slash(\\)")
+print("Algoritma\nPemograman")
+print("Algoritma\bPemograman")
+print("Algoritma\tPemograman")
+print("Algoritma\rPemograman")
+#tipe data Integer
+print(20)
+#tipe data Float
+print(3.14)
+print(.2)
+print(4.2e-3)
+#tipe data Binary
+print(0b10)
+#tipe data octal
+print(0o10)
+#tipe data Hexadecimal
+print('tipe data heksa desimal',0x10)
+#tipe data Complex
+print(5j)
+#tipe data List
+print([1,2,3,4,5,])
+print(["satu","dua","tiga"])
+#tipe data Tuple
+print((1,2,3,4,5))
+print(("satu","dua","tiga"))
+#tipe data Dictionary
+print({"nama":"Budi",'umur':20})
+#tipe data Dictionary dimasukan ke dalam variabel biodata
+biodata = {"nama":"Budi",'umur':20}
+print(biodata)
+type(biodata)

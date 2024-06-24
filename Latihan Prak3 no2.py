049626c26e36999b6f9ffe976297b6c834462824
JackHunter2 <46602010+JackHunter2@users.noreply.github.com>
2024-06-24 23:32:03 +0700

    Create Latihan Prak3 no2.py

---
+++ Latihan Prak3 no2.py
@@ -0,0 +1,14 @@
+#Biodata
+deskripsi ="ini program python"
+nama = "Abdul Rachman"
+alamat = "Bandung"
+umur = 23
+hobi = "Bermain Game"
+
+print("Nama saya ",nama," yang beralamat di ",alamat," umur sekarang ",umur," tahun dan memiliki hobi ",hobi)
+# menghitung luas & keliling persegi panjang
+panjang = 15
+lebar = 5.7
+keliling_persegi_panjang = panjang*lebar
+lebar_persegi_panjang = 2*(panjang+lebar)
+print("Sebuah persegi panjang memiliki panjang ",panjang,"cm, lebar ",lebar,"cm dan memiliki luas sebesar ",keliling_persegi_panjang,"cm dan keliling ",lebar_persegi_panjang,"cm")

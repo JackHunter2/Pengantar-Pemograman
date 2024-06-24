73d9cb621f12666c6d01fdd3c985bba2ad97c42b
JackHunter2 <46602010+JackHunter2@users.noreply.github.com>
2024-06-24 23:29:49 +0700

    Create Latihan Prak3 no 1.py

---
+++ Latihan Prak3 no 1.py
@@ -0,0 +1,34 @@
+nama = "Abdul Rachman"
+print(nama)
+
+age = 23
+print(age)
+print(type(age))
+age = "Dua Puluh Tiga"
+print(age)
+print(type(age))
+
+first_name = "Abdul"
+middle_name = "Rachman"
+last_name = "Ibrahim"
+name = first_name+" "+middle_name+" "+last_name
+age = 23
+hobby = "Bermain Game"
+print("Profil\n",name,"\n",age,"\n",hobby)
+
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+AGE = 8
+print(age,Age,aGe,AGE,a_g_e,age,age,AGE)
+
+numberofcollegegraduates = 2500
+NUMBEROFCOLLEGEGRADUATES = 2500
+numberOfCollegeGraduates = 2500
+NumberOfCollegeGraduates = 2500
+number_of_college_graduates = 2500
+print(numberofcollegegraduates,NUMBEROFCOLLEGEGRADUATES,numberOfCollegeGraduates,NumberOfCollegeGraduates,number_of_college_graduates)
